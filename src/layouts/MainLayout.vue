363d09f736f43d3d36244b562769a9c743d5c817
amirhossein <ak47.amirhosseinkhanmohamadi@gmail.com>
2024-09-27 23:30:02 +0330

    add styling  and layout for mobile

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,7 +7,13 @@
 
         <q-toolbar-title class="text-weight-bold">
 
-          Twitter
+          <span class="gt-xs">Twitter</span>
+          
+            <q-icon class="q-py-md q-px-lg  lt-sm absolute-center" 
+             color="blue-8"
+             name="fa-brands fa-twitter" 
+             size="3rem" />
+      
         </q-toolbar-title>
 
 
@@ -43,30 +49,49 @@
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
       <!-- drawer content -->
-      <q-input  
-       dense
-       placeholder="Search Twitter"
-      :rounded="true" 
-       :outlined="true"
-       color="positive"
-      class="q-ma-md">
+      <q-input dense placeholder="Search Twitter" :rounded="true" :outlined="true" color="positive" class="q-ma-md">
         <template v-slot:prepend>
           <q-icon name="search" />
         </template>
       </q-input>
 
-      <q-list  padding>
-      <q-item>
-        <q-item-section>
-          <q-item-label overline>OVERLINE</q-item-label>
-          <q-item-label>Single line item</q-item-label>
-          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-        </q-item-section>
+      <q-list :separator="true" padding>
+        <q-item class="q-pa-lg">
+          <q-item-section>
+            <q-item-label overline class="text-h3 text-teal-9 text-bold">learning</q-item-label>
+            <q-item-label>Full course will be start</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit
+              elit.</q-item-label>
+          </q-item-section>
 
-        <q-item-section side top>
-          <q-item-label caption>5 min ago</q-item-label>
-        </q-item-section>
-      </q-item>
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-lg">
+          <q-item-section>
+            <q-item-label overline class="text-h3 text-teal-9 text-bold">Sport</q-item-label>
+            <q-item-label>Match will be begin</q-item-label>
+            <q-item-label caption>barcelona and rea madrid will play soon</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>10 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-lg">
+          <q-item-section>
+            <q-item-label overline class="text-h3 text-teal-9 text-bold">News</q-item-label>
+            <q-item-label>Euro increase value agains Dollar</q-item-label>
+            <q-item-label caption>in stock market the value of Euro increase</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>25 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
 
     </q-drawer>
