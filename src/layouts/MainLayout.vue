e962f197166b000cae254f18cf620b1e763ad9c8
amirhossein <ak47.amirhosseinkhanmohamadi@gmail.com>
2024-09-27 22:58:55 +0330

    add content to right side

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -55,6 +55,20 @@
         </template>
       </q-input>
 
+      <q-list  padding>
+      <q-item>
+        <q-item-section>
+          <q-item-label overline>OVERLINE</q-item-label>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+      </q-list>
+
     </q-drawer>
 
     <q-page-container>
