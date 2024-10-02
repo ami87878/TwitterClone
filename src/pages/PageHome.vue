151890fd2fc438f7008d6546b1eba013e200acc7
amirhossein khanmohammadi <ak47.amirhosseinkhanmohamadi@gmail.com>
2024-10-02 20:59:59 +0330

    add delete add and other stuff functionality

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -17,7 +17,7 @@
         </template>
 
         <template #after>
-          <q-btn class="q-mt-xl" :disable="!text" color="primary" round dense flat icon="send" />
+          <q-btn @click="addTwitter" class="q-mt-xl" :disable="!text" color="primary" round dense flat icon="send" />
         </template>
       </q-input>
     </div>
@@ -26,41 +26,40 @@
 
     <q-list separator>
 
+      <transition-group appear enter-active-class="animated fadeIn slow"
+        leave-active-class="animated fadeOut slow">
+        <q-item class="q-py-lg  " v-for="message in tweitter" :key="message.date">
+          <q-item-section avatar :top="true">
+            <q-avatar>
+              <img src="../assets/myImage.jpg">
+            </q-avatar>
+          </q-item-section>
+  
+          <q-item-section>
+            <q-item-label>
+              <strong class="text-weight-bold q-ma-sm">Amirhossein Khanmohammadi</strong>
+              <span class="text-grey-7">@amirhossein &bull; {{ targetDate(message.date) }}</span>
+            </q-item-label>
+            <q-item-label class="q-pa-md twitter--space">
+              {{ message.content }}
+            </q-item-label>
+            <div class='row justify-between q-mt-md'>
+              <q-btn flat color="blue-grey-4" size='sm' round icon="comment" />
+              <q-btn flat color="blue-grey-4" size='sm' round icon="fas fa-retweet" />
+              <q-btn flat color="blue-grey-4" size='sm' round icon="fas fa-heart" />
+  
+              <q-btn @click="removeFunc(message)" flat color="blue-grey-4" size='sm' round icon="fas fa-trash" />
+            </div>
+          </q-item-section>
+  
+          <!-- <q-item-section side top>
+            {{}}
+          </q-item-section> -->
+        </q-item>
 
-      <q-item 
-      class="q-py-lg" 
-      
-       v-for="message in tweitter"
-       :key="message.date"
-       >
-        <q-item-section avatar :top="true" >
-          <q-avatar >
-            <img  src="../assets/myImage.jpg">
-          </q-avatar>
-        </q-item-section>
+      </transition-group>
 
-        <q-item-section>
-          <q-item-label >
-            <strong class="text-weight-bold q-ma-sm">Amirhossein Khanmohammadi</strong>
-            <span class="text-grey-7">@amirhossein</span>
-          </q-item-label>
-          <q-item-label class="q-pa-md twitter--space"> 
-           {{ message.content }}
-          </q-item-label>
-          <div class='row justify-between q-mt-md'>
-            <q-btn flat color="blue-grey-4" size='sm' round  icon="comment" />
-            <q-btn flat color="blue-grey-4" size='sm' round  icon="fas fa-retweet" />
-            <q-btn flat color="blue-grey-4" size='sm' round  icon="fas fa-heart" />
-            <q-btn flat color="blue-grey-4" size='sm' round  icon="fas fa-trash" />
-          </div>
-        </q-item-section>
-
-        <q-item-section side top>
-          {{Date.now()}}
-        </q-item-section>
-      </q-item>
-
-      <q-separator  />
+      <q-separator />
 
     </q-list>
   </q-page>
@@ -68,7 +67,7 @@
 
 
 <script>
-
+import { formatDistance } from "date-fns";
 
 export default {
   name: 'PageHome',
@@ -76,9 +75,10 @@ export default {
     return {
       logic: true,
       text: '',
+      addDisappre:false,
       tweitter: [
         {
-          date:1234567890,
+          date: 1727793453744 ,
           content:
           `Lorem ipsum dolor sit
            amet consectetur adipisicing 
@@ -88,33 +88,46 @@ export default {
           `
         },
         {
-          date:1234567891,
+          date: 1728893453743 ,
           content:'Lorem ipsum dolor sit amet consecteturamet consectetur adipisicing elit. Est, earum.'
         },
-        {
-          date:1234567892,
-          content:'  dolor sit amet consectetur adipisicing Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, earum.'
-        },
-        {
-          date:1234567893,
-          content:'amet consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, earum.'
-        },
-        {
-          date:1234567894,
-          content:'elit. Est, earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, earum.'
-        },
+        
+        
       ],
 
+      
 
 
     }
   },
 
-  methods: {
+  methods:{ // like computed property
+    targetDate(value){
 
+      return formatDistance((value),new Date)
+      
+    },
+    addTwitter(){
+     
+      const twMsg={
+        date:Date.now(),
+        content:this.text,
+      }
+      this.tweitter.unshift(twMsg)
+      this.text=''
+    },
+    removeFunc(msg) {
 
+      
+      this.addDisappre=true
+      this.tweitter=this.tweitter.filter((item)=>item.date!==msg.date)
+      
+
+    }
   },
+  
   watch: {
+
 
   }
 
@@ -129,4 +142,21 @@ export default {
 .twitter--space{
   white-space: pre-line
 }
+.appeare{
+  animation: shown 0.3s ease-in ;
+}
+
+
+@keyframes shown {
+  
+  0%{
+    opacity: 0;
+    transform: translateY(-1rem)
+  };
+  100%{
+    opacity: 1;
+    transform: translateY(0rem)
+  };
+}
+
 </style>
